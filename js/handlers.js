430d2b9a38446d17af67489172985e14530f72df
Pauline Keith Zipagan <paulinekeith.zipagan@infor.com>
2026-06-28 15:16:53 +0800

    fixed re-rendering of likes

--- js/handlers.js
+++ js/handlers.js
@@ -7,8 +7,14 @@ export function eventHandlers(){
 
    postContainer.addEventListener('click', (event) => {
     if (event.target.classList.contains('like-btn')){
-        posts[event.target.dataset.index].likes++;
-        renderPosts();
+      const index = event.target.dataset.index;      
+      posts[index].likes++;
+
+      //instead of rerendering the whole posts
+      //goal is to rerender only the likes counts
+      //   renderPosts();
+      const likesCount = event.target.closest('footer').querySelector('.like-count h3');
+      likesCount.textContent = `${posts[index].likes} likes`
     }
    })
    
