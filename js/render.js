802f6a8eaf37bc17505ff0247ef6a348e22d810f
Pauline Keith Zipagan <paulinekeith.zipagan@infor.com>
2026-06-28 15:32:13 +0800

    data rendering and code cleanup

--- js/render.js
+++ js/render.js
@@ -7,29 +7,31 @@ export function renderPosts(){
     // console.log(postContainer)
 
     postContainer.innerHTML = posts.map((post, index) =>
-    `<div class="article-header">
-        <img class="user-avatar" src="${post.avatar}" alt="">
-        <div class="user-details">
-            <h2>${post.name}</h2>
-            <p>${post.location}</p>
+    `<article>
+        <div class="article-header">
+            <img class="user-avatar" src="${post.avatar}" alt="${post.avatarAlt}">
+            <div class="user-details">
+                <h2>${post.name}</h2>
+                <p>${post.location}</p>
+            </div>
         </div>
-    </div>
-    <figure>
-        <img class="post-image" src="${post.post}" alt="">
-    </figure>
-    <footer>
-        <div class="buttons">
-            <img class="btn like-btn" data-index="${index}" src="images/icon-heart.png" alt="">
-            <img class="btn" src="images/icon-comment.png" alt="">
-            <img class="btn" src="images/icon-dm.png" alt="">
-        </div>
-        <div class="like-count">
-            <h3>${post.likes} likes</h3>
-        </div>
-        <div class="post-details">
-            <p><span class="post-username">${post.username}&nbsp;&nbsp;</span>${post.comment}</p>
-        </div>
-    </footer>
+        <figure>
+            <img class="post-image" src="${post.post}" alt="${post.postAlt}">
+        </figure>
+        <footer>
+            <div class="buttons">
+                <img class="btn like-btn" data-index="${index}" src="images/icon-heart.png" alt="">
+                <img class="btn" src="images/icon-comment.png" alt="">
+                <img class="btn" src="images/icon-dm.png" alt="">
+            </div>
+            <div class="like-count">
+                <h3>${post.likes} likes</h3>
+            </div>
+            <div class="post-details">
+                <p><span class="post-username">${post.username}</span>${post.comment}</p>
+            </div>
+        </footer>
+    </article>
     `).join('')
 
 }
